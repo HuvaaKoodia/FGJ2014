8efe1a71644f4be206f76d070691f91aedcde5d5
ihQprinsessa <kat12smiikkal@kamk.fi>
2014-01-26 15:55:23 +0200

    audio imp + stuffz

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,126 +2,132 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
-	public ResourceStore ResStore;
-	public List<UnitMain> Units=new List<UnitMain>();
+    public ResourceStore ResStore;
+    public List<UnitMain> Units = new List<UnitMain> ();
     public HudController Hud;
+    public BaseMain ABase, BBase, CBase, DBase;
+    public AudioSource audio_src;
+    public int AmountOfDeaths = 0, AmountOfSpawns = 0, AmountOfDeathsLastMin = 0, AmountOfSpawnsLastMin = 0;
+    public float SecondsAfterStart = 0, FingerOfGodRadius = 2.5f;
+    float LastMin = 0;
 
-	public BaseMain ABase,BBase,CBase,DBase;
+    public void AddDeath (UnitMain unit)
+    {
+        ++AmountOfDeaths;
+        ++AmountOfDeathsLastMin;
 
-	public int AmountOfDeaths=0,AmountOfSpawns=0,AmountOfDeathsLastMin=0,AmountOfSpawnsLastMin=0;
-    public float SecondsAfterStart=0,FingerOfGodRadius=2.5f;
-	float LastMin=0;
+        Units.Remove (unit);
+    }
 
-	public void AddDeath(UnitMain unit){
-		++AmountOfDeaths;
-		++AmountOfDeathsLastMin;
-
-        Units.Remove(unit);
-	}
-	public void AddSpawn(){
-		++AmountOfSpawns;
-		++AmountOfSpawnsLastMin;
-	}
+    public void AddSpawn ()
+    {
+        ++AmountOfSpawns;
+        ++AmountOfSpawnsLastMin;
+    }
 
     Timer ScoreTimer;
+    int score = 0, score_last = 0, multi = 2;
 
-    int score=0,score_last=0,multi=2;
-
-    void AddScore(int amount){
-        score+=amount*multi;
-        score_last+=amount;
+    void AddScore (int amount)
+    {
+        score += amount * multi;
+        score_last += amount;
 
     }
 
-    void HudScoresUpdate(){
+    void HudScoresUpdate ()
+    {
         //calculate scores
-        AddScore(1000);
+        AddScore (1000);
 
-        Hud.SetScore(score);
-        Hud.SetMulti(multi);
-        Hud.SetScoreAdd(score_last,multi);
+        Hud.SetScore (score);
+        Hud.SetMulti (multi);
+        Hud.SetScoreAdd (score_last, multi);
 
-        score_last=0;
+        score_last = 0;
     }
 
-	// Use this for initialization
-	void Start () {
-        ScoreTimer=new Timer(5000,HudScoresUpdate);
-        Hud.SetScore(score);
-        Hud.SetMulti(multi);
+    // Use this for initialization
+    void Start ()
+    {
+        ScoreTimer = new Timer (5000, HudScoresUpdate);
+        Hud.SetScore (score);
+        Hud.SetMulti (multi);
 
 
-		//generate units
+        //generate units
 
-		int a=Subs.GetRandom(5,10);
-		for(int i=0;i<a;i++){
-			ABase.AddUnit();
-		}
+        int a = Subs.GetRandom (5, 10);
+        for (int i=0; i<a; i++) {
+            ABase.AddUnit ();
+        }
 
-		a=Subs.GetRandom(5,10);
-		for(int i=0;i<a;i++){
-			BBase.AddUnit();
-		}
+        a = Subs.GetRandom (5, 10);
+        for (int i=0; i<a; i++) {
+            BBase.AddUnit ();
+        }
 
-		a=Subs.GetRandom(5,10);
-		for(int i=0;i<a;i++){
-			CBase.AddUnit();
-		}
+        a = Subs.GetRandom (5, 10);
+        for (int i=0; i<a; i++) {
+            CBase.AddUnit ();
+        }
 
-		a=Subs.GetRandom(5,10);
-		for(int i=0;i<a;i++){
-			DBase.AddUnit();
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        ScoreTimer.Update();
+        a = Subs.GetRandom (5, 10);
+        for (int i=0; i<a; i++) {
+            DBase.AddUnit ();
+        }
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        ScoreTimer.Update ();
 
-		SecondsAfterStart+=Time.deltaTime;
-		LastMin+=Time.deltaTime;
-		if (LastMin>60){
-			LastMin=0;
-			AmountOfDeathsLastMin=AmountOfSpawnsLastMin=0;
-		}
+        SecondsAfterStart += Time.deltaTime;
+        LastMin += Time.deltaTime;
+        if (LastMin > 60) {
+            LastMin = 0;
+            AmountOfDeathsLastMin = AmountOfSpawnsLastMin = 0;
+        }
 
-		//input
+        //input
 
-		int mask=1<<LayerMask.NameToLayer("SpeechBubble");
-        int unit_mask=1<<LayerMask.NameToLayer("Unit");
+        int mask = 1 << LayerMask.NameToLayer ("SpeechBubble");
+        int unit_mask = 1 << LayerMask.NameToLayer ("Unit");
 
-		if (Input.GetMouseButtonDown(0)){
-			var hit=Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero,1,mask);
-			
-			if (hit.collider!=null){
-				var bubble=hit.collider.gameObject.GetComponent<SpeechbubbleMain>();
-				if (bubble.StatementPhase)
-					bubble.PlayerApprove();
-			}
-		}
+        if (Input.GetMouseButtonDown (0)) {
+            var hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero, 1, mask);
+            
+            if (hit.collider != null) {
+                var bubble = hit.collider.gameObject.GetComponent<SpeechbubbleMain> ();
+                if (bubble.StatementPhase)
+                    bubble.PlayerApprove ();
+            }
+        }
 
-		if (Input.GetMouseButtonDown(1)){
-			var hit=Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero,1,mask);
-			
-			if (hit.collider!=null){
-				var bubble=hit.collider.gameObject.GetComponent<SpeechbubbleMain>();
-				if (bubble.StatementPhase)
-					bubble.PlayerDissapprove();
-			}
-		}
+        if (Input.GetMouseButtonDown (1)) {
+            var hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero, 1, mask);
+            
+            if (hit.collider != null) {
+                var bubble = hit.collider.gameObject.GetComponent<SpeechbubbleMain> ();
+                if (bubble.StatementPhase)
+                    bubble.PlayerDissapprove ();
+            }
+        }
 
-        if (Input.GetKey(KeyCode.Space)){
-            var units = Physics2D.OverlapCircleAll (Camera.main.ScreenToWorldPoint(Input.mousePosition), FingerOfGodRadius, unit_mask);
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            var units = Physics2D.OverlapCircleAll (Camera.main.ScreenToWorldPoint (Input.mousePosition), FingerOfGodRadius, unit_mask);
 
             foreach (var u in units) {
                 var unit = u.GetComponent<UnitMain> ();
                 if (unit != null) {
-                    unit.Die();
+                    unit.Die ();
                 }
             }
-
+            audio_src.PlayOneShot (audio_src.clip);
         }
 
 #if UNITY_EDITOR
@@ -148,15 +154,16 @@ public class GameController : MonoBehaviour {
             DebugGUIOn=!DebugGUIOn;
         }
 #endif
-	}
+    }
 
-    public bool DebugGUIOn=false;
+    public bool DebugGUIOn = false;
 
-    public float GetAmountOfMaxPopulation(Nationality Nat){
+    public float GetAmountOfMaxPopulation (Nationality Nat)
+    {
         //DEV. Opti. save all results in a batch every 5 seconds of so.
-        int amount=0;
-        foreach(var u in Units){
-            if (u.MyNationality==Nat){
+        int amount = 0;
+        foreach (var u in Units) {
+            if (u.MyNationality == Nat) {
                 ++amount;
             }
         }
@@ -164,15 +171,17 @@ public class GameController : MonoBehaviour {
         return amount;
     }
 
-	public float GetPercentOfMaxPopulation(Nationality Nat){
-		return GetAmountOfMaxPopulation(Nat)/Units.Count;
-	}
+    public float GetPercentOfMaxPopulation (Nationality Nat)
+    {
+        return GetAmountOfMaxPopulation (Nat) / Units.Count;
+    }
 
-    public float GetAmountOfMaxPopulation(Ideology ide){
+    public float GetAmountOfMaxPopulation (Ideology ide)
+    {
         //DEV. Opti. save all results in a batch every 5 seconds of so.
-        int amount=0;
-        foreach(var u in Units){
-            if (u.MyIdeology==ide){
+        int amount = 0;
+        foreach (var u in Units) {
+            if (u.MyIdeology == ide) {
                 ++amount;
             }
         }
@@ -180,11 +189,12 @@ public class GameController : MonoBehaviour {
         return amount;
     }
     
-    public float GetPercentOfMaxPopulation(Ideology ide){
-        return GetAmountOfMaxPopulation(ide)/Units.Count;
+    public float GetPercentOfMaxPopulation (Ideology ide)
+    {
+        return GetAmountOfMaxPopulation (ide) / Units.Count;
     }
 
-    string guitext="";
+    string guitext = "";
 
     void OnGUI ()
     {
@@ -192,17 +202,17 @@ public class GameController : MonoBehaviour {
             return;
         
         guitext = "Game stats:\n";
-        guitext+="Amount of Fruit= "+Units.Count;
-        guitext+="\nNationality:\n";
+        guitext += "Amount of Fruit= " + Units.Count;
+        guitext += "\nNationality:\n";
         foreach (var i in Subs.EnumValues<Nationality>()) {
-            guitext += i + ": Amount=" + GetAmountOfMaxPopulation(i) + ", Percent= " + GetPercentOfMaxPopulation(i)*100f + "%";
+            guitext += i + ": Amount=" + GetAmountOfMaxPopulation (i) + ", Percent= " + GetPercentOfMaxPopulation (i) * 100f + "%";
             guitext += "\n";
         }
-        guitext+="\nIdealogy:\n";
+        guitext += "\nIdealogy:\n";
         foreach (var i in Subs.EnumValues<Ideology>()) {
-            guitext += i + ": Amount=" + GetAmountOfMaxPopulation(i) + ", Percent= " + GetPercentOfMaxPopulation(i)*100f + "%";
+            guitext += i + ": Amount=" + GetAmountOfMaxPopulation (i) + ", Percent= " + GetPercentOfMaxPopulation (i) * 100f + "%";
             guitext += "\n";
         }
-        GUI.Box (new Rect (Screen.width-300, 10, 300, 400), guitext);
+        GUI.Box (new Rect (Screen.width - 300, 10, 300, 400), guitext);
     }
 }
